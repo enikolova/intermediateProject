82b15cec9c7d10b186b2c71157f596b30d1204a7
Elena Nikolova <elena.nikolova9756@abv.bg>
2017-09-23 17:23:11 +0300

    added menu

---
+++ proj/asets/script/menu.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(){
+  var li=document.querySelectorAll('#aside-menu li');
+ 
+  li.forEach(function(x){
+      var menu= document.getElementById(x.id+'-menu')
+      menu.addEventListener('mouseover',function(){
+          menu.style.display='inline-block';
+      })
+       menu.addEventListener('mouseleave',function(){
+          menu.style.display='none';
+      })
+      x.addEventListener('mouseover',function(){
+        x.style.borderTop='0.5px solid lightgrey';
+         x.style.borderBottom='0.5px solid lightgrey';
+          x.style.borderRight='none'
+           menu.style.display='inline-block';
+          event.stopImmediatePropagation()
+           console.log(menu);
+      });
+      x.addEventListener('mouseout',function(){
+          x.style.border='none';
+           menu.style.display='none';
+          console.log(menu);
+      })
+  })
+
+
+},false)
