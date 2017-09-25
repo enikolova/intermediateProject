dcce5de6074e4dca7f270436616a55616fda42b7
Elena Nikolova <elena.nikolova9756@abv.bg>
2017-09-25 12:13:29 +0300

    edited

--- proj/asets/script/logIn.js
+++ proj/asets/script/logIn.js
@@ -15,4 +15,33 @@ document.addEventListener('DOMContentLoaded',function(){
     children[0].addEventListener('mouseleave',function(){
         log.style.display='none';
     })
+    var fav=document.querySelector('#favorite');
+    fav.addEventListener('mouseover',function(){
+        fav.style.display='inline-block';
+    })
+    fav.addEventListener('mouseleave',function(){
+        fav.style.display='none';
+    })
+    children[1].addEventListener('mouseover',function(event){
+        fav.style.display='inline-block';
+        
+    });
+    children[1].addEventListener('mouseleave',function(){
+        fav.style.display='none';
+    })
+     var cart=document.querySelector('#cart');
+   cart.addEventListener('mouseover',function(){
+       cart.style.display='inline-block';
+    })
+   cart.addEventListener('mouseleave',function(){
+        cart.style.display='none';
+    })
+    children[2].addEventListener('mouseover',function(event){
+       cart.style.display='inline-block';
+        
+    });
+    children[2].addEventListener('mouseleave',function(){
+        cart.style.display='none';
+    })
+
 })
