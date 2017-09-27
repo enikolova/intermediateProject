05659c3a42097a994ede4cf7e30345bfcdef1025
unknown <elena.nikolova9756@abv.bg>
2017-09-27 19:03:39 +0300

    login and registration form

---
+++ proj/asets/script/reg.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function(){
+document.querySelector('#register').addEventListener('click',function(){
+    event.preventDefault();
+    document.getElementById('login').style.display='none';
+    document.getElementById('reg').style.display='block';
+})
+})
